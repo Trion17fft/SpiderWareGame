9ae8ac7724ad4aef13b6c750a14ee644c78216d5
Trion17fft <evanlemaire@icloud.com>
2026-09-03 00:17:20 +0200

    variables

---
+++ global.gd
@@ -0,0 +1,28 @@
+extends Node
+
+
+var minigames_cp = 0 # nombre de level finis
+var lives = 7 # nombre de vies
+var exp = 0 # experiences
+var money = 100 # argents
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
