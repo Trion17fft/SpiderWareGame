fdcd38d57b7339e77058e5d44ee090606cfff527
Trion17fft <evanlemaire@icloud.com>
2026-09-06 19:55:17 +0200

    web collector

--- scenes/minigame_1.gd
+++ scenes/minigame_1.gd
@@ -1,32 +1,41 @@
 extends Node2D
-@onready var timer: RichTextLabel = $timer #literally just the richlabeltext
+@onready var timer: RichTextLabel = $timer
 
+var Web_collected = 0
 var time : float
+var finished = false # empêche de déclencher la transition plusieurs fois
 
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	await Timer(30)
-	
-	get_tree().change_scene_to_file("res://scenes/scene.tscn") # changes your scene
+	Timer(30) # on lance le minuteur SANS await, pour ne pas bloquer _ready()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	timer.text = str(snapped(time, 0.10)) # this makes names easier
+	timer.text = str(snapped(time, 0.10))
 
-func Timer(start_time: float): # making a new function for timer countdown!
-	# we want the timer to go down, and when it reaches 0 it transitions 
-	# to the next scene!
-	
+	if finished:
+		return # on ne fait plus rien une fois la transition lancée
+
+	if Web_collected >= 3: # victoire, vérifiée à CHAQUE frame
+		finished = true
+		if Global.minigames_done > 3:
+			get_tree().change_scene_to_file("res://scenes/done_screen.tscn")
+		else:
+			get_tree().change_scene_to_file("res://scenes/timer_screen.tscn")
+
+	elif time <= 0.0: # défaite (temps écoulé sans les 3 toiles)
+		finished = true
+		Global.minigames_done -= 1
+		Global.lives -= 1
+		get_tree().change_scene_to_file("res://scenes/level_scene.tscn")
+
+func Web_collect() -> void:
+	Web_collected += 1
+
+func Timer(start_time: float):
 	time = start_time
-	
-	while time > 0.0: # run if timer hasnt reached 0
+	while time > 0.0:
 		await wait(0.10)
-		time = time - 0.10
-	
-	#when timer reaches 0
+		time -= 0.10
 	return
-	
-func wait(seconds: float) -> void: # write this simple function out for wait!
-	await get_tree().create_timer(seconds).timeout # makes u wait
+
+func wait(seconds: float) -> void:
+	await get_tree().create_timer(seconds).timeout
--- scenes/web.gd
+++ scenes/web.gd
@@ -11,7 +11,7 @@ func _ready() -> void:
 	self_area.area_entered.connect(_on_area_entered)
 
 func _on_area_entered(area: Area2D) -> void:
-	if area.is_in_group("Player") and self.visible:
+	if area.is_in_group("player") and self.visible:
 		emit_signal("Web_collected")
 		self.hide()
 
